3628f561a929b5d98cb2a04ee5c0083219208582
pedrobarretto <pedro@barretto.com.br>
2021-10-24 17:09:44 -0300

    First commit

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPdgkchEg4hoED46lt_cmxKSlr8u7GOdA",
+  authDomain: "fir-crud-12530.firebaseapp.com",
+  projectId: "fir-crud-12530",
+  storageBucket: "fir-crud-12530.appspot.com",
+  messagingSenderId: "552952191119",
+  appId: "1:552952191119:web:30d8c1549d52f78fcf78fa",
+  measurementId: "G-1BDNZBPLG2"
+};
+
+const app = initializeApp(firebaseConfig);
